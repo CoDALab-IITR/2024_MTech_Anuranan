f4cbb135b9b417553b30c9806430a7422b45e0a9
Anuranan das <64675845+Hakoder@users.noreply.github.com>
2023-07-02 20:28:12 +0530

    Update heuristic_qc.py

--- heuristic_qc.py
+++ heuristic_qc.py
@@ -1,30 +1,26 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit                     #Qiskit tools importing
 from qiskit import *
-from mqt import qmap
+from mqt import qmap                                  #MQT tools of QMAP importing
 
-arch = qmap.Architecture(5,{
+arch = qmap.Architecture(5,{                          #5 qubit manual random arbitrary quantum architecture formed
     (1, 0), (2, 0), 
     (2, 1), (3, 2), 
     (3, 4), (2, 4)})
 
-f1 = open("input.txt","r")
+f1 = open("input.txt","r")                            #input file of quantum circuit is called
 
 c=1
-for line in f1:
-    f2 = open("output.txt","a")
+for line in f1:                                       #automation process
+    f2 = open("output.txt","a")                       #output file for storing visualization of heuristic mapping of quantum circuit with 5 qubit quantum architecture
     print("initial circuit ",c)
-    circ = QuantumCircuit.from_qasm_str(line)
-    f2.write(circ)
+    circ = QuantumCircuit.from_qasm_str(line)         #forming quantum circuit
+    f2.write(circ)                                    #storing quantum circuit in output file
     f2.write("\n")
-    #print(circ)
-    #print("\n")
     print("heuristic circuit ",c)
-    qc_m , res = qmap.compile(circ, arch , method="heuristic")
-    f2.write(qc_m)
+    qc_m , res = qmap.compile(circ, arch , method="heuristic")            #forming heuristic mapping quantum circuit
+    f2.write(qc_m)                                                        #storing heuristic mapping quantum circuit in output file
     f2.write("\n\n\n\n")
-    #print(qc_m)
-    #print("\n\n\n\n\n")
     c+=1
-    f2.close()
+    f2.close()                                        #closing file
     
-f1.close()
+f1.close()                                            #closing file
